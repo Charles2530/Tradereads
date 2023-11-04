a611bf2df3e05f89d1c9a39da81efe7b74ea1282
charles <2569337619@qq.com>
2023-11-04 18:34:48 +0800

    0.0.0 project initial with front_end vue3

---
+++ front_end/src/api/user.js
@@ -0,0 +1,19 @@
+import request from "../utils/require";
+
+//get
+export function getUser(params) {
+  return request({
+    url: "/user/list",
+    method: "get",
+    params,
+  });
+}
+
+//post
+export function editUser(params) {
+  return request({
+    url: "/user/edit",
+    method: "post",
+    params,
+  });
+}
