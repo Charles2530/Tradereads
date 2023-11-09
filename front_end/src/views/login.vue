a3978143fab63fcad4b616072183b4bc3cf534f1
charles <2569337619@qq.com>
2023-11-09 15:02:06 +0800

    0.0.2 完成部分动态代码书写(Login)

--- front_end/src/views/login.vue
+++ front_end/src/views/login.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <div class="login">
     <div
       class="login__container p-8 bg-white rounded shadow-lg max-w-md w-full"
@@ -6,23 +6,26 @@
       <h1 class="login__title text-2xl font-bold mb-6 bg-black">Login</h1>
       <form class="login__form">
         <div class="login__text mb-4">
-          <input
-            type="text"
-            placeholder="Username"
+          <el-input
             class="w-full p-2 border rounded border-gray-300 focus:outline-none focus:border-blue-500"
+            v-model="loginInfo.username"
+            placeholder="Username"
           />
         </div>
         <div class="login__password mb-4">
-          <input
+          <el-input
+            v-model="loginInfo.password"
+            class="w-full p-2 border rounded border-gray-300 focus:outline-none focus:border-blue-500"
             type="password"
             placeholder="Password"
-            class="w-full p-2 border rounded border-gray-300 focus:outline-none focus:border-blue-500"
+            show-password
           />
         </div>
         <div class="login__button">
           <button
             type="submit"
             class="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200"
+            @click.prevent="Login"
           >
             Login
           </button>
@@ -30,19 +33,53 @@
       </form>
     </div>
   </div>
-</template> -->
-<template>
-  <el-input v-model="input" placeholder="Please input" />
 </template>
 
 <script>
 import { ref } from "vue";
 export default {
-  setup() {
-    const input = ref("");
-    return {
-      input,
-    };
+  data: () => ({
+    loginInfo: {
+      username: ref(""),
+      password: ref(""),
+    },
+  }),
+  methods: {
+    printLogin() {
+      console.log(this.loginInfo.username);
+      console.log(this.loginInfo.password);
+    },
+    Login() {
+      const vm = this;
+      const valid = this.$refs.loginForm.validate();
+      if (!valid) return;
+      this.$axios({
+        method: "post",
+        url: "/login",
+        data: {
+          username: this.loginInfo.username,
+          password: this.loginInfo.password,
+        },
+      })
+        .then(function (response) {
+          console.log(response);
+          if (response.data.status == 200) {
+            vm.$message({
+              message: "登录成功",
+              type: "success",
+            });
+            vm.$router.push("/home");
+          } else {
+            vm.$message({
+              message: "登录失败",
+              type: "error",
+            });
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
   },
 };
 </script>
