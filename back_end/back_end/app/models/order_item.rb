bf8fc2c0e9d66312922bbb88748eb07960bc0184
黄泓亮 <21371227@buaa.edu.cn>
2023-11-30 22:28:15 +0800

    change chinese

--- back_end/back_end/app/models/order_item.rb
+++ back_end/back_end/app/models/order_item.rb
@@ -3,4 +3,5 @@ class OrderItem < ApplicationRecord
   belongs_to :order
 
   validates :number, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :state, inclusion:{ in: %w(待支付 待发货 待收货 已完成) }
 end
