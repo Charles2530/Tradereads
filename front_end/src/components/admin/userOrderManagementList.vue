c031ca1aed136c82a3340e4588e8ae80d3fffbaa
charles <2569337619@qq.com>
2023-12-09 17:09:31 +0800

    美化UI

--- front_end/src/components/admin/userOrderManagementList.vue
+++ front_end/src/components/admin/userOrderManagementList.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="order-list-page">
+  <div class="order-list-page mt-20">
     <h1 class="text-4xl font-bold mb-8 flex justify-center">订单列表</h1>
     <div class="search-box">
-      <el-input v-model="searchQuery" placeholder="搜索用户订单"></el-input>
+      <el-row>
+        <div class="mx-2">
+          <el-icon class="mt-2"><Search /></el-icon>
+        </div>
+        <div>
+          <el-input v-model="searchQuery" placeholder="搜索用户订单"></el-input>
+        </div>
+      </el-row>
     </div>
     <div class="bg-gray-600/80 p-4 rounded-lg shadow-md">
-      <el-scrollbar max-height="680px">
+      <el-scrollbar max-height="600px">
         <order-item-admin
           v-for="order in filteredOrders"
           :key="order.order_id"
@@ -64,7 +71,44 @@ export default {
 </script>
 
 <style scoped>
-.search-box {
-  margin: 20px 5px 20px 15px;
+.order-list-page {
+  margin: 0 auto;
+  padding: 20px;
 }
+
+h1 {
+  color: #333;
+  font-family: "Helvetica Neue", sans-serif;
+}
+
+.search-box {
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+.search-box el-input {
+  max-width: 500px;
+  width: 100%;
+  transition: all 0.3s ease;
+}
+
+.search-box el-input:hover {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+}
+
+.order-item-admin {
+  background-color: #fff;
+  border: 1px solid #eaeaea;
+  border-radius: 8px;
+  margin-bottom: 15px;
+  padding: 15px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+  transition: all 0.3s ease;
+}
+
+.order-item-admin:hover {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+/* 其他需要的样式 */
 </style>
--- front_end/src/components/order/OrderItemAdmin.vue
+++ front_end/src/components/order/OrderItemAdmin.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="p-4">
     <div class="flex flex-col bg-white shadow-md p-6 rounded-lg">
       <div class="pb-4 border-b border-gray-200 mb-4">
@@ -67,6 +67,124 @@
           </div>
         </div>
       </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { deleteOrder } from "@/api/order.js";
+export default {
+  name: "OrderItemAdmin",
+  props: {
+    order_id: {
+      type: Number,
+      required: true,
+    },
+    buyer_id: {
+      type: Number,
+      required: true,
+    },
+    total_price: {
+      type: Number,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+    order_time: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    deleteThisOrder() {
+      deleteOrder(this.order_id)
+        .then((res) => {
+          console.log(res);
+          if (res.success) {
+            ElMessage({
+              message: "删除成功",
+              type: "success",
+            });
+          } else {
+            ElMessage({
+              message: res.message,
+              type: "error",
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      window.location.reload();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.order-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem; /* 调整间距，根据需要调整 */
+}
+
+.order-item {
+  width: calc(50% - 1rem); /* 计算 50% 宽度，并减去间距 */
+  box-sizing: border-box;
+}
+</style> -->
+<template>
+  <div class="p-4">
+    <div class="flex flex-col bg-white shadow-md p-6 rounded-lg">
+      <!-- Existing order header code -->
+      <div class="pb-4 border-b border-gray-200 mb-4">
+        <el-row :gutter="22">
+          <el-col :span="6">
+            <div class="mt-2">
+              <span class="text-gray-600 text-2xl font-bold"
+                >买家: {{ buyer_id }}</span
+              >
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="mt-2">
+              <span class="text-gray-600 text-2xl font-bold"
+                >总价: {{ total_price }}</span
+              >
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="mt-2">
+              <span class="text-gray-600 text-2xl font-bold"
+                >下单时间: {{ order_time }}</span
+              >
+            </div>
+          </el-col>
+          <el-col :span="2">
+            <div class="mt-2">
+              <el-popconfirm
+                title="Are you sure to delete this?"
+                @confirm="deleteThisOrder"
+              >
+                <template #reference>
+                  <el-button type="danger">删除订单</el-button>
+                </template>
+              </el-popconfirm>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+
+      <!-- el-table for order items -->
+      <el-table :data="items" style="width: 100%">
+        <el-table-column prop="product_name" label="产品名称"></el-table-column>
+        <el-table-column prop="product_price" label="价格"></el-table-column>
+        <el-table-column prop="buy_num" label="数量"></el-table-column>
+        <el-table-column prop="sell_address" label="发往"></el-table-column>
+        <el-table-column prop="state" label="订单状态"></el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
