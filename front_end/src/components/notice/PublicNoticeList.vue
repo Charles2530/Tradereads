74a0257b3ed4a20543d5b6588aa92d5af9cfac1b
charles <2569337619@qq.com>
2023-12-09 17:42:19 +0800

    完成公告栏改造

--- front_end/src/components/notice/PublicNoticeList.vue
+++ front_end/src/components/notice/PublicNoticeList.vue
@@ -21,6 +21,7 @@
 
 <script>
 export default {
+  name: "PublicNoticeList",
   props: {
     books: {
       type: Array,
--- front_end/src/views/notice/index.vue
+++ front_end/src/views/notice/index.vue
@@ -8,10 +8,17 @@
     <div class="container mx-auto p-4" style="position: relative; z-index: 0">
       <div class="mb-4">
         <h1 class="text-3xl font-semibold mb-2 text-white">二手书交易公告栏</h1>
-        <p class="text-gray-600 notice-detail">浏览最新的二手书信息。</p>
+        <p class="text-gray-400 notice-detail">浏览最新的二手书信息。</p>
       </div>
       <div>
-        <notice-list :books="books" />
+        <el-tabs v-model="activeName" class="demo-tabs">
+          <el-tab-pane label="公共通知" name="first"
+            ><public-notice-list :books="notices"
+          /></el-tab-pane>
+          <el-tab-pane label="我的关注" name="Config">
+            <follow-notice-list :books="books" />
+          </el-tab-pane>
+        </el-tabs>
       </div>
     </div>
   </div>
@@ -19,8 +26,10 @@
 
 <script setup>
 import { ref } from "vue";
-import NoticeList from "@c/notice/NoticeList.vue";
-const books = ref([
+import PublicNoticeList from "@c/notice/PublicNoticeList.vue";
+import FollowNoticeList from "@c/notice/FollowNoticeList.vue";
+const activeName = ref("first");
+const notices = ref([
   {
     id: 1,
     title: "Book Title 1",
@@ -34,11 +43,33 @@ const books = ref([
     date: "2023-02-01",
   },
 ]);
+const books = ref([
+  {
+    id: 3,
+    title: "Book Title 3",
+    description: "Book Description 1",
+    date: "2023-01-01",
+  },
+  {
+    id: 4,
+    title: "Book Title 4",
+    description: "Book Description 2",
+    date: "2023-02-01",
+  },
+]);
 </script>
 
 <style>
 .notice-detail:hover {
-  color: #fff;
+  color: #fff !important;
   text-decoration: underline;
+}
+
+.el-tabs__item {
+  color: #fff !important;
+}
+
+.el-tabs__item.is-active {
+  color: #409eff !important;
 }
 </style>
