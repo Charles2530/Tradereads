a74122f735b56f319c9f7b8ca65d528ab4d98af7
charles <2569337619@qq.com>
2023-12-11 09:01:37 +0800

    完成接口编写

--- front_end/src/views/notice/index.vue
+++ front_end/src/views/notice/index.vue
@@ -10,6 +10,16 @@
         <h1 class="text-3xl font-semibold mb-2 text-white">二手书交易公告栏</h1>
         <p class="text-gray-400 notice-detail">浏览最新的二手书信息。</p>
       </div>
+      <!-- <div v-if="store.getRight === 1"> -->
+      <div>
+        <el-button
+          @click="showCreateNoticeDialog"
+          plain
+          class="btn-create-notice"
+        >
+          发布公告
+        </el-button>
+      </div>
       <div>
         <el-tabs v-model="activeName" class="demo-tabs">
           <el-tab-pane label="公共通知" name="first"
@@ -22,16 +32,54 @@
       </div>
     </div>
   </div>
+  <div>
+    <el-dialog v-model="createNoticeDialog" title="发布公告" width="50%">
+      <el-form
+        :model="newNotice"
+        ref="createNoticeForm"
+        :rules="createNoticeFormRules"
+      >
+        <el-form-item label="标题" prop="title">
+          <el-input v-model="newNotice.title"></el-input>
+        </el-form-item>
+        <el-form-item label="内容" prop="content">
+          <el-input type="textarea" v-model="newNotice.content"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <div class="dialog-footer">
+        <el-button @click="createNoticeDialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="createNoticeFunc">发布</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup>
 import { onMounted, ref, computed } from "vue";
-import { showCurrentNotices } from "@/api/notice.js";
+import { userStore } from "@/store/user.js";
+import { createNotice, showCurrentNotices } from "@/api/notice.js";
 import PublicNoticeList from "@c/notice/PublicNoticeList.vue";
 import FollowNoticeList from "@c/notice/FollowNoticeList.vue";
 const activeName = ref("first");
 const AllNotice = ref([]);
+const store = userStore();
 onMounted(() => {
+  getNotices();
+});
+
+const notices = computed(() => {
+  return AllNotice.value.filter((item) => {
+    return item.notice_type === 0;
+  });
+});
+
+const books = computed(() => {
+  return AllNotice.value.filter((item) => {
+    return item.notice_type === 1;
+  });
+});
+const getNotices = () => {
   showCurrentNotices().then((res) => {
     if (res.success) {
       console.log(res.data.notice);
@@ -44,17 +92,54 @@ onMounted(() => {
       });
     }
   });
-});
-const notices = computed(() => {
-  return AllNotice.value.filter((item) => {
-    return item.notice_type === 0;
+};
+
+const createNoticeDialog = ref(false);
+const newNotice = {
+  title: "",
+  content: "",
+};
+const createNoticeFormRules = {
+  title: [
+    { required: true, message: "请输入标题", trigger: "blur" },
+    { min: 1, max: 20, message: "长度在 1 到 20 个字符", trigger: "blur" },
+  ],
+  content: [
+    { required: true, message: "请输入内容", trigger: "blur" },
+    { min: 1, max: 100, message: "长度在 1 到 100 个字符", trigger: "blur" },
+  ],
+};
+
+const showCreateNoticeDialog = () => {
+  createNoticeDialog.value = true;
+};
+
+const createNoticeFunc = () => {
+  const form = ref("createNoticeForm");
+  form.value.validate(async (valid) => {
+    if (valid) {
+      try {
+        createNotice(newNotice).then((res) => {
+          if (res.success) {
+            console.log(res.data.notice);
+            getNotices();
+            newNotice.title = "";
+            newNotice.content = "";
+            createNoticeDialog.value = false;
+          } else {
+            ElMessage({
+              showClose: true,
+              type: "error",
+              message: res.message,
+            });
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
   });
-});
-const books = computed(() => {
-  return AllNotice.value.filter((item) => {
-    return item.notice_type === 1;
-  });
-});
+};
 </script>
 
 <style>
@@ -69,5 +154,14 @@ const books = computed(() => {
 
 .el-tabs__item.is-active {
   color: #409eff !important;
+}
+
+.btn-create-notice {
+  background-color: #409eff;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 4px;
+  cursor: pointer;
 }
 </style>
