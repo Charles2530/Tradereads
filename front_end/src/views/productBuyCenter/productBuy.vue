a0168dd00989adb0d5a9c7bd59740041d4f3f4d8
Xuan <113332272+Forget1015@users.noreply.github.com>
2023-12-15 21:34:49 +0800

    Update productBuy.vue

--- front_end/src/views/productBuyCenter/productBuy.vue
+++ front_end/src/views/productBuyCenter/productBuy.vue
@@ -23,6 +23,11 @@
     <div class="main" :style="onehidden">
       <div class="header">
         <p>商品中心</p>
+        <div class="search2">
+          <label for="search"></label>
+          <input type="text" class="ss0" v-model="search" v-on:focus="searchfind()" v-on:blur="disappear()">
+        </div>
+        <div class="ss4"></div>
         <div class="return">
           <router-link to="/personalCenter"></router-link>
         </div>
@@ -43,7 +48,7 @@
       </div>
       <div class="button">
         <buyProductItem v-for="shoppinglist in shoopingUser.value" :key="shoppinglist.product_id"
-          :product_id="shoppinglist.product_id" :product_name="shoppinglist.product_name" :price="parseInt(shoppinglist.price)"
+          :product_id="shoppinglist.product_id" :product_name="shoppinglist.product_name" :price="shoppinglist.price"
           :product_press="shoppinglist.product_press" :seller_name="shoppinglist.seller_name"
           :sell_address="shoppinglist.sell_address" :product_type="shoppinglist.product_type"
           :product_image="shoppinglist.product_image">
@@ -56,31 +61,38 @@
       <div class="up">
         <div class="total">
           <label for="price">price</label>
-          <input type="text" style="font-size: 20px" v-model="price" id="price" autocomplete="off" />
+          <input type="text" style="font-size: 20px" v-model="price" id="price" autocomplete="off"
+            v-on:focus="searchfind1()" v-on:blur="disappear1()" />
         </div>
         <div class="total">
           <label for="sell_address">address</label>
-          <input type="text" style="font-size: 20px" v-model="sell_address" id="sell_address" autocomplete="off" />
+          <input type="text" style="font-size: 20px" v-model="sell_address" id="sell_address" autocomplete="off"
+            v-on:focus="searchfind2()" v-on:blur="disappear2()" />
         </div>
         <div class="total">
           <label for="store">store</label>
-          <input type="text" style="font-size: 20px" v-model="store" id="store" autocomplete="off" />
+          <input type="text" style="font-size: 20px" v-model="store" id="store" autocomplete="off"
+            v-on:focus="searchfind3()" v-on:blur="disappear3()" />
         </div>
         <div class="total">
           <label for="product_name">name</label>
-          <input type="text" style="font-size: 20px" v-model="product_name" id="product_name" autocomplete="off" />
+          <input type="text" style="font-size: 20px" v-model="product_name" id="product_name" autocomplete="off"
+            v-on:focus="searchfind4()" v-on:blur="disappear4()" />
         </div>
         <div class="total">
           <label for="number">image</label>
-          <input type="text" style="font-size: 20px" v-model="product_image" id="number" autocomplete="off" />
+          <input type="text" style="font-size: 20px" v-model="product_image" id="number" autocomplete="off"
+            v-on:focus="searchfind5()" v-on:blur="disappear5()" />
         </div>
         <div class="total">
           <label for="product_press">press</label>
-          <input type="text" style="font-size: 20px" v-model="product_press" id="product_press" autocomplete="off" />
+          <input type="text" style="font-size: 20px" v-model="product_press" id="product_press" autocomplete="off"
+            v-on:focus="searchfind6()" v-on:blur="disappear6()" />
         </div>
         <div class="total">
           <label for="product_type">type</label>
-          <input type="text" style="font-size: 20px" v-model="product_type" id="product_type" autocomplete="off" />
+          <input type="text" style="font-size: 20px" v-model="product_type" id="product_type" autocomplete="off"
+            v-on:focus="searchfind7()" v-on:blur="disappear7()" />
         </div>
         <div class="and">
           <button class="left" @click="cancel()">
@@ -114,6 +126,45 @@
   font-size: 60px;
   color: aqua;
 }
+
+.search2 {
+  position: absolute;
+  float: left;
+  top: 20px;
+  width: 400px;
+  right: 930px;
+
+
+}
+
+.ss0 {
+  width: 600px;
+  height: 60px;
+  font-size: 35px;
+  box-shadow: 0 12px 12px rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(83, 71, 161);
+  border-radius: 30px;
+  text-align: center;
+  background-color: rgb(255, 255, 255);
+}
+
+.ss4 {
+  position: absolute;
+  float: left;
+  top: 25px;
+  width: 400px;
+  right: 380px;
+  font-family: 'icomoon';
+  font-size: 20px;
+  content: "\e987";
+  font-size: 30px;
+}
+
+.ss4:hover {
+  color: red;
+  font-size: 40px;
+}
+
 
 .left {
   position: fixed;
@@ -260,6 +311,7 @@ export default {
       twohidden: "visibility:hidden",
       total: "",
       shoopingUser: [],
+      search: "点击搜索",
       project_title: "DataBase Project",
       provinces: [
         { name: '默认', cities: ['null'] },
@@ -371,8 +423,76 @@ export default {
       showAllProducts({ "show_following": this.followapp, "show_order_base": this.show_order_base, "show_order": this.show_order }).then(res => {
         this.shoopingUser.value = res.data.products;
       });
-    }
+    },
+    searchfind(event) {
+      this.search = "";
+    },
+    disappear(event) {
+      this.search = "点击搜索";
+    },
+    searchfind1(event) {
+      this.price = "";
+    },
+    disappear1(event) {
+      if (this.price == "") {
+        this.price = "请输入价格（请输入实数）";
+      }
 
+    },
+    searchfind2(event) {
+      this.sell_address = "";
+    },
+    disappear2(event) {
+      if (this.sell_address == "") {
+        this.sell_address = "请输入发货地址";
+      }
+
+    },
+    searchfind3(event) {
+      this.store = "";
+    },
+    disappear3(event) {
+      if (this.store == "") {
+        this.store = "请输入库存（请输入正整数）";
+      }
+
+    },
+    searchfind4(event) {
+      this.product_name = "";
+    },
+    disappear4(event) {
+      if (this.product_name == "") {
+        this.product_name = "请输入商品名";
+      }
+
+    },
+    searchfind5(event) {
+      this.product_image = "";
+    },
+    disappear5(event) {
+      if (this.product_image == "") {
+        this.product_image = "请输入照片地址（请输入 1.jpg / 2.jpg / 3.jpg / 4.jpg）";
+      }
+
+    },
+    searchfind6(event) {
+      this.product_press = "";
+    },
+    disappear6(event) {
+      if (this.product_press == "") {
+        this.product_press = "请输入出版社地址";
+      }
+
+    },
+    searchfind7(event) {
+      this.product_type = "";
+    },
+    disappear7(event) {
+      if (this.product_type == "") {
+        this.product_type = "请输入书类（请输入 art/math/literature/history/novel）";
+      }
+
+    },
   },
   mounted() {
     showAllProducts({ "show_following": 0, "show_order_base": '', "show_order": '' }).then(res => {
